f3508d724041a4208a11255f25d44da87c246885
Reda <redaus@protonmail.com>
2021-04-07 01:47:03 +0200

    Made some changes to the game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,11 +1,15 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.dsl.EntityBuilder;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.physics.CollisionHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Game extends GameApplication {
 
@@ -24,9 +28,22 @@ public class Game extends GameApplication {
         // Creating player
         player = FXGL.entityBuilder()
                 .at(400, 400)
-                .view("koen_warner.png")
+                .viewWithBBox("koen_warner.png")
+                .with(new CollidableComponent(true))
                 .scale(2, 2)
+                .type(EntityTypes.PLAYER)
                 .buildAndAttach();
+
+        FXGL.getGameTimer().runAtInterval(() -> {
+            int randomPosition = ThreadLocalRandom.current().nextInt(80, FXGL.getGameScene().getAppWidth() - 80);
+
+            FXGL.entityBuilder().at(randomPosition, randomPosition)
+                    .viewWithBBox(new Rectangle(10, 10, Color.BLUE))
+                    .with(new CollidableComponent(true))
+                    .type(EntityTypes.BLOCK)
+                    .buildAndAttach();
+        }, Duration.millis(2000));
+
 
         FXGL.getGameScene().setBackgroundColor(Color.ORANGE);
     }
@@ -44,6 +61,16 @@ public class Game extends GameApplication {
         });
         FXGL.onKey(KeyCode.D, () -> {
             player.translateX(3);
+        });
+    }
+
+    @Override
+    protected void initPhysics() {
+        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.PLAYER, EntityTypes.BLOCK) {
+            @Override
+            protected void onCollision(Entity player, Entity block) {
+                block.removeFromWorld();
+            }
         });
     }
 
